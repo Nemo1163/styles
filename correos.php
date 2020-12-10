026fde5c9355e2bed8909a659280f462763d8db8
Carlos Carrillo <carrillocarlos376@gmail.com>
2020-12-10 12:24:08 -0600

    cambios de estilos

---
+++ correos.php
@@ -0,0 +1,18 @@
+<?php
+
+$destinatario = 'contacto.jomar@gmail.com';
+$nombre = $_POST['nombre'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+$email = $_POST['email'];
+
+
+$header = "Enviado desde la pagina jomar soluciones";
+$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+
+mail($destinatario, $asunto , $mensajeCompleto , $header);
+echo "<script>alert('correo enviado exitosamente')</script>";
+echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
+?>
